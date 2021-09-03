1a1420d48dc1ec961a3e5989caba0416859ff8cf
chander123 <chanderbhushan8819@gmail.com>
2021-09-03 10:37:11 +0530

    Fixed some issues

--- src/Plugin/Block/ProductListingBlock.php
+++ src/Plugin/Block/ProductListingBlock.php
@@ -3,12 +3,15 @@
 namespace Drupal\product_listing\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\node\Entity\Node;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use BaconQrCode\Renderer\ImageRenderer;
 use BaconQrCode\Renderer\Image\ImagickImageBackEnd;
 use BaconQrCode\Renderer\RendererStyle\RendererStyle;
 use BaconQrCode\Writer;
-use Drupal\Core\StreamWrapper\PublicStream;
+use Drupal\Core\Routing\RouteMatchInterface;
 
 /**
  * Provides a 'Product listing' Block.
@@ -19,7 +22,68 @@ use Drupal\Core\StreamWrapper\PublicStream;
  *   category = @Translation("Product Listing"),
  * )
  */
-class ProductListingBlock extends BlockBase {
+class ProductListingBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Constructs a new ProductListingBlock instance.
+   *
+   * @param array $configuration
+   *   The plugin configuration, i.e. an array with configuration values keyed
+   *   by configuration option name. The special key 'context' may be used to
+   *   initialize the defined contexts by setting it to an array of context
+   *   values keyed by context names.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The current route match.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, ModuleHandlerInterface $module_handler, RouteMatchInterface $route_match) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+    $this->moduleHandler = $module_handler;
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('module_handler'),
+      $container->get('current_route_match')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -30,16 +94,14 @@ class ProductListingBlock extends BlockBase {
       new ImagickImageBackEnd()
     );
     $writer = new Writer($renderer);
-    $node = \Drupal::routeMatch()->getParameter('node');
-    $node = Node::load($node->id());
-    $writer->writeFile($node->get('field_qr_code_link')->uri, PublicStream::basePath() . '/qrcode_' . $node->id() .'.png');
-    $product_list[$node->id()]['product_title'] = $node->getTitle();
-    $product_list[$node->id()]['product_des'] = $node->body_value;
-    $product_list[$node->id()]['product_image'] = $node->get('field_product_image')->entity->url();
-    $product_list[$node->id()]['product_qr_link'] = 'public://qrcode_' . $node->id() .'.png';
+    $node = $this->routeMatch->getParameter('node');
+    $module_path = $this->moduleHandler->getModule('product_listing')->getPath() . '/qr_code_images';
+    $node = $this->entityTypeManager->getStorage('node')->load($node->id());
+    $writer->writeFile($node->get('field_qr_code_link')->uri, $module_path . '/qrcode_' . $node->id() . '.png');
+    $product_qr_code = file_create_url($module_path . '/qrcode_' . $node->id() . '.png');
     return [
       '#theme' => 'product_listing_block',
-      '#product_list' => $product_list,
+      '#product_qr_code' => $product_qr_code,
       '#cache' => [
         'max-age' => 0,
       ],
